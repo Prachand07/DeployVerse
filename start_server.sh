8469b0af3336928e733e572c67a9ef918cb4ab5b
Aarush Luthra <163556661+Prachand07@users.noreply.github.com>
2025-01-28 01:15:33 +0530

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-# Navigate to the application directory
-cd /home/ec2-user/react-app
-
-# Start the React app
-echo "Starting the React app..."
-nohup npm start > /dev/null 2>&1 &
-
-# Check if the app started successfully
-if [ $? -eq 0 ]; then
-    echo "React app started successfully."
-else
-    echo "Failed to start the React app."
-    exit 1
-fi
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo systemctl restart nginx
