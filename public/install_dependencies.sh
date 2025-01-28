9f1e932a8ce16ab336d61088cf787f4b5c1c99b2
Aarush Luthra <163556661+Prachand07@users.noreply.github.com>
2025-01-28 22:59:57 +0530

    Update install_dependencies.sh

--- public/install_dependencies.sh
+++ public/install_dependencies.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-
-# Update the system
 sudo yum update -y
-
-# Install Nginx
-sudo amazon-linux-extras enable nginx1
-sudo yum install -y nginx
-
-# Install Node.js and npm
-sudo yum install -y nodejs npm
-
-# Enable and start Nginx
-sudo systemctl enable nginx
-sudo systemctl start nginx
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo "Deployment complete and server restarted successfully."
